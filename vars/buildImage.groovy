1de19276bf88c3b68a237ae5ff715bf8aad4b491
JayanthSGowda <jayanth.gowda2807@gmail.com>
2024-09-12 22:18:10 +0530

    passing the image name

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(){
+def call(String imageName){
     echo 'building docker image and pushing it to the repo...'
      withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-         sh 'docker build -t jaynths/jayanth:jma-3.0 .'
+         sh "docker build -t ${imageName} ."
          sh 'echo $PASS | docker login -u $USER --password-stdin'
-         sh 'docker push jaynths/jayanth:jma-3.0'
+         sh "docker push ${imageName}"
      }
 }
