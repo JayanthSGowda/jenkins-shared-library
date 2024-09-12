e2ab2672287b4d99c7f03184bfa0a29a07a13784
JayanthSGowda <jayanth.gowda2807@gmail.com>
2024-09-12 21:42:15 +0530

    first commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+def call(){
+    echo 'building docker image and pushing it to the repo...'
+     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+         sh 'docker build -t jaynths/jayanth:jma-2.0 .'
+         sh 'echo $PASS | docker login -u $USER --password-stdin'
+         sh 'docker push jaynths/jayanth:jma-2.0'
+     }
+}
